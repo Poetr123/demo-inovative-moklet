94e26f85dc3719cac95077bb01daa0d9d7298a4d
Poetr123 <150533614+Poetr123@users.noreply.github.com>
2025-09-04 18:56:43 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'telkom-red': '#e11d48',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
