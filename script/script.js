9a9b81e6242d4769870fb1caa98797cc2a14e55b
Poetr123 <150533614+Poetr123@users.noreply.github.com>
2025-09-05 18:31:22 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -353,10 +353,11 @@
                             email: this.register.email,
                             password: this.register.password,
                             options: {
+                                emailRedirectTo: 'https://demo-inovative-moklet.vercel.app/auth/callback.html'
                                 data: {
                                     full_name: this.register.name
                                 }
-                                emailRedirectTo: 'https://demo-inovative-moklet.vercel.app/auth/callback.html'
+                               
                             }
                         });
                         
